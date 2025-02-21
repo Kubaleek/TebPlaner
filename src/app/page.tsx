744b8f3c3a97b2345704bab4d1c537f4c31ad5ec
Kuba <kuba.krol.ldz@gmail.com>
2025-02-21 20:22:47 +0100

    poprawki i przygotowanie komponentów do logowania

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,62 @@
 import { DotPattern } from '@/components/magicui/dot-pattern';
-import { cn } from '@/lib/utils';
+import { cn, nowCopyRight } from '@/lib/utils';
+
 export default function Home() {
+  const DotArrays = [
+    {
+      id: 1,
+      className: '[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] fill-blue-800  text-blue-800',
+    },
+    {
+      id: 2,
+      className:  '[mask-image:linear-gradient(to_bottom_left,white,transparent,transparent)] fill-blue-800  text-blue-800',
+    },
+    {
+      id: 3,
+      className: '[mask-image:linear-gradient(to_top_left,white,transparent,transparent)] fill-red-800 text-red-800',
+    },
+    {
+      id: 4,
+      className: '[mask-image:linear-gradient(to_top_right,white,transparent,transparent)] fill-red-800 text-red-800',
+    }
+  ]
+  const nowYear = nowCopyRight();
+
   return (
     <div className="grid grid-cols-12 min-h-screen">
-      <div className="col-span-12 lg:col-span-5 xl:col-span-4 order-2 lg:order-1 flex flex-col justify-between p-6">
-        <div className='flex flex-col gap-5'>
-          <h2 className="text-3xl">
+      <div className="col-span-12 md:col-span-5 lg:col-span-5 bg-zinc-200  xl:col-span-4 order-2 md:order-1 gap-6 flex flex-col justify-between p-6">
+        <div className="flex flex-col gap-3 md:gap-5">
+          <h2 className="text-2xl md:text-3xl">
             Teb <span className="text-red-600">Technikum</span> i{' '}
             <span className="text-blue-600">Liceum</span>
           </h2>
-          <div className='flex flex-col gap-3'>
-            <h3 className="text-4xl  televisor:text-5xl televisor:font-bold">
+          <div className="flex flex-col md:gap-3.5">
+            <h3 className="text-xl md:text-3xl lg:text-4xl  televisor:text-5xl televisor:font-semibold">
               Logowanie
             </h3>
-            <p className="televisor:text-2xl text-pretty leading-relaxed font-medium">
-              Zaloguj się, aby sprawdzić plan lekcji, ogłoszenia oraz inne
-              ważne informacje. Po zalogowaniu uzyskasz dostęp do aktualnych
-              zajęć i funkcji aplikacji.
+            <p className="televisor:text-xl text-pretty leading-relaxed font-normal">
+              Zaloguj się, aby sprawdzić plan lekcji, ogłoszenia oraz inne ważne
+              informacje. Po zalogowaniu uzyskasz dostęp do aktualnych zajęć i
+              funkcji aplikacji.
             </p>
           </div>
         </div>
-        <div />
+        <p className="text-zinc-800 televisor:text-xl font-normal">
+          © {nowYear} Teb <span className="text-red-600">Technikum</span> i{' '}
+          <span className="text-blue-600">Liceum</span> Wszelkie prawa
+          zastrzeżone.
+        </p>
       </div>
-      <div className="col-span-12 lg:col-span-7  xl:col-span-8  w-full order-1 lg:order-2 h-full lg:h-screen relative flex flex-col items-start gap-6 televisor:gap-8 p-6 justify-center overflow-hidden">
-        <DotPattern
-          width={20}
-          height={20}
-          cx={1}
-          cy={1}
-          cr={1}
-          opacity={1}
-          className={cn(
-            '[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] fill-blue-800  text-blue-800',
-          )}
-        />
-        <DotPattern
-          width={20}
-          height={20}
-          cx={1}
-          cy={1}
-          cr={1}
-          opacity={1}
-          className={cn(
-            '[mask-image:linear-gradient(to_top_left,white,transparent,transparent)] fill-red-800 text-red-800',
-          )}
-        />
+      <div className="col-span-12 md:col-span-7 lg:col-span-7  xl:col-span-8  w-full order-1 md:order-2 h-full lg:h-screen relative hidden md:flex flex-col items-start gap-6 televisor:gap-8 p-6 justify-center overflow-hidden">
+        {DotArrays.map((dot) => (
+          <DotPattern key={dot.id} width={20} height={20} cx={1} cy={1} cr={1} opacity={1} className={cn(dot.className)} />
+        ))}
         <div className="flex flex-col gap-4 televisor:gap-5 backdrop-blur-3xl z-10 p-3">
           <h1 className="text-4xl  televisor:text-7xl televisor:font-bold">
             Teb <span className="text-red-600">Technikum</span> i{' '}
             <span className="text-blue-600">Liceum</span> Panel
           </h1>
-          <p className="televisor:text-2xl text-pretty televisor:px-2 leading-relaxed font-medium">
+          <p className="televisor:text-2xl text-pretty televisor:px-2 leading-relaxed font-normal">
             To{' '}
             <span className="text-red-600">
               system wyświetlający plany lekcji i ogłoszenia na szkolnych
