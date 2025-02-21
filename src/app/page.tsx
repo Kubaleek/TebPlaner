cf9d36f3e68e7a5a807eb5ebeb4fb9cdc271df2b
Kuba <kuba.krol.ldz@gmail.com>
2025-02-21 17:06:01 +0100

    początki logowania panelu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,50 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="grid grid-cols-12 min-h-screen">
+      <div className="col-span-12 lg:col-span-5 xl:col-span-4"></div>
+      
+      <div className="col-span-12 lg:col-span-7  xl:col-span-8 h-screen relative flex flex-col items-start gap-6 p-6 justify-center overflow-hidden">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-4xl televisor:text-5xl televisor:font-bold">
+            Teb <span className="text-red-600">Technikum</span> i <span className="text-blue-600">Liceum</span> Panel
+          </h1>
+          <p className="televisor:text-2xl text-pretty leading-relaxed font-medium">
+            To system wyświetlający plany lekcji i ogłoszenia na szkolnych telewizorach. Uczniowie i nauczyciele mogą szybko sprawdzać informacje, a administracja zarządza danymi przez intuicyjny panel. Umożliwia bieżące aktualizacje, wysyłanie komunikatów i organizowanie harmonogramu zajęć.
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="flex flex-col lg:flex-row justify-start max-w-5xl gap-6 items-start">
+          <div className="bg-zinc-200/25 backdrop-blur-3xl p-4 border border-[#1818185b] shadow-[0px_37px_10px_0px_rgba(0,0,0,0.00),_0px_24px_10px_0px_rgba(0,0,0,0.01),_0px_13px_8px_0px_rgba(0,0,0,0.02),_0px_6px_6px_0px_rgba(0,0,0,0.03),_0px_1px_3px_0px_rgba(0,0,0,0.04)]">
+            <div />
+            <div>
+              <h2 className="text-xl font-bold">Rozmawiaj na czacie ze swoimi połączeniami</h2>
+              <p className="text-gray-500 text-sm leading-relaxed">To nie ma sensu, ale mamy to tutaj. Używaj tego tak, jak chcesz.</p>
+            </div>
+          </div>
+          <div className="bg-zinc-200/25 backdrop-blur-3xl p-4 border border-[#1818185b] shadow-[0px_37px_10px_0px_rgba(0,0,0,0.00),_0px_24px_10px_0px_rgba(0,0,0,0.01),_0px_13px_8px_0px_rgba(0,0,0,0.02),_0px_6px_6px_0px_rgba(0,0,0,0.03),_0px_1px_3px_0px_rgba(0,0,0,0.04)]">
+            <div />
+            <div>
+              <h2 className="text-xl font-bold">Rozmawiaj na czacie ze swoimi połączeniami</h2>
+              <p className="text-gray-500 text-sm leading-relaxed">To nie ma sensu, ale mamy to tutaj. Używaj tego tak, jak chcesz.</p>
+            </div>
+          </div>
+          <div className="bg-zinc-200/25 backdrop-blur-3xl p-4 border border-[#1818185b] shadow-[0px_37px_10px_0px_rgba(0,0,0,0.00),_0px_24px_10px_0px_rgba(0,0,0,0.01),_0px_13px_8px_0px_rgba(0,0,0,0.02),_0px_6px_6px_0px_rgba(0,0,0,0.03),_0px_1px_3px_0px_rgba(0,0,0,0.04)]">
+            <div />
+            <div>
+              <h2 className="text-xl font-bold">Rozmawiaj na czacie ze swoimi połączeniami</h2>
+              <p className="text-gray-500 text-sm leading-relaxed">To nie ma sensu, ale mamy to tutaj. Używaj tego tak, jak chcesz.</p>
+            </div>
+          </div>
+        </div>
+        {/* <div className="max-w-2xl flex flex-col gap-6 mx-auto relative z-10 w-full">
+          <h1 className="lg:text-5xl xl:text-7xl font-bold text-white">
+            Teb Planer Szkolna
+          </h1>
+          <p className="font-medium lg:text-lg xl:text-xl text-neutral-300 max-w-3xl">
+            Teb Planer Lekcyjny to aplikacja wyświetlająca plany lekcji i ogłoszenia dla nauczycieli na telewizorach w szkołach. Umożliwia łatwe zarządzanie informacjami dzięki systemowi do wgrywania danych i wysyłania wiadomości.
+          </p>
+        </div> */}
+      </div>
     </div>
   );
 }
