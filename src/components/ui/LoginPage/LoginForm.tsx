20f4a594d08cf1e2251b868e300620ccebfa8f2b
Kuba <kuba.krol.ldz@gmail.com>
2025-02-26 18:46:22 +0100

    zrobienie wyglądu loginPage

--- src/components/ui/LoginPage/LoginForm.tsx
+++ src/components/ui/LoginPage/LoginForm.tsx
@@ -1,9 +1,10 @@
 import { DotPattern } from '@/components/magicui/dot-pattern';
 import { GridPattern } from '@/components/magicui/grid-pattern';
-import { Label } from '@/components/shadcn/label';
 import { nowCopyRight } from '@/lib/utils';
-import { Input } from '@/components/shadcn/input';
 import { Button } from '@/components/shadcn/button';
+import Image from 'next/image';
+import TebLogo from '../../../../public/teblarge.png';
+import { Lock, Mail } from 'lucide-react';
 
 export default function LoginForms() {
   const nowYear = nowCopyRight();
@@ -11,54 +12,56 @@ export default function LoginForms() {
   return (
     <div className="col-span-12 lg:col-span-6 h-full flex z-[0] lg:h-screen relative flex-col justify-center items-center loginBackground border-solid">
       <DotPattern className="stroke-[#ff828a] fill-[#ff828a] [mask-image:radial-gradient(300px_circle_at_top_left,white,transparent)] xl:[mask-image:radial-gradient(800px_circle_at_top_left,white,transparent)]" />
-      <GridPattern className="stroke-[#ff828a] fill-[#ff828a] [mask-image:radial-gradient(300px_circle_at_bottom_right,white,transparent)] xl:[mask-image:radial-gradient(600px_circle_at_bottom_right,white,transparent)]" />
-      <div className="border border-[#ff828a] shadow-lg flex flex-col lg:w-fit lg:p-10 gap-8 w-full md:m-5 lg:max-w-screen-md z-[1] rounded-xl mx-auto justify-between items-center backdrop-blur-md bg-opacity-50 bg-[#ff828a] p-6">
-        <div className="flex flex-col gap-6">
-          <div className="flex items-center align-middle gap-2 text-center">
-            <h3 className="text-4xl televisor:text-7xl text-[#fafafa] font-semibold">
-              Logowanie
+      <GridPattern
+        x={50}
+        className="stroke-[#ff828a] fill-[#ff828a] [mask-image:radial-gradient(300px_circle_at_bottom_right,white,transparent)] xl:[mask-image:radial-gradient(600px_circle_at_bottom_right,white,transparent)]"
+      />
+      <div className="relative border bg-[#ff828a] flex-col justify-center flex items-center lg:justify-start lg:items-center bg-opacity-5 border-[#ff828a] backdrop-blur-3xl w-full lg:w-[400px] lg:h-[600px]  xl:w-[500px] h-full xl:h-[600px] rounded-md z-[1] lg:py-12 p-6">
+        <DotPattern className="w-32 absolute top-0 left-0 p-4 h-32 text-[#fafafa]" />
+        <DotPattern className="w-32 absolute top-auto left-auto p-4 h-32 text-[#fafafa]" />
+        <div className="flex flex-col gap-8 w-full max-w-xs">
+          <div className="flex items-center flex-col gap-1">
+            <Image
+              src={TebLogo}
+              alt="Teb Technikum i Liceum Logo"
+              className="w-20 h-20"
+            />
+            <h3 className="text-4xl lg:text-4xl televisor:text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-br  from-white to-[#fafafa] bg-opacity-50">
+              Planer Teb
             </h3>
           </div>
-          <div className="flex flex-col gap-8">
-            <form className="flex flex-col gap-5">
-              <div className="flex flex-col gap-4">
-                <Label
-                  htmlFor="email"
-                  className="text-2xl televisor:text-3xl font-medium text-[#fafafa]"
-                >
-                  Email
-                </Label>
-                <Input
+          <div className='flex flex-col gap-5'>
+            <form className="flex flex-col gap-6">
+              <div className="flex">
+                <span className="inline-flex items-center px-3 text-sm text-red-900 bg-red-200 border rounded-e-0 border-red-300 border-e-0 rounded-s-md">
+                  <Mail className="w-4 h-4 text-red-500" />
+                </span>
+                <input
+                  type="text"
                   id="email"
-                  type="email"
-                  className="bg-[#ff828a] backdrop:blur-lg text-[#fafafa] text-lg lg:text-xl rounded-md televisor:text-2xl p-3    focus:ring-2 focus:ring-[#ff828a] focus:outline-none"
+                  className="rounded-none rounded-e-lg bg-red-50 border text-red-900 placeholder-red-400 focus:outline-none text-base  block flex-1 min-w-0 w-full border-red-300 p-2.5"
+                  placeholder="lodz@teb-edukacja.pl"
                 />
               </div>
-              <div className="flex flex-col gap-3">
-                <Label
-                  htmlFor="password"
-                  className="text-2xl televisor:text-3xl font-medium text-[#fafafa] blur-1"
-                >
-                  Hasło
-                </Label>
-                <Input
+              <div className="flex">
+                <span className="inline-flex items-center px-3 text-sm text-red-900 bg-red-200 border rounded-e-0 border-red-300 border-e-0 rounded-s-md">
+                  <Lock className="w-4 h-4 text-red-500" />
+                </span>
+                <input
+                  type="text"
                   id="password"
-                  type="password"
-                  className="bg-[#ff828a] backdrop:blur-lg text-[#fafafa] text-lg lg:text-xl rounded-md televisor:text-2xl p-3    focus:ring-2 focus:ring-[#ff828a] focus:outline-none"
+                  className="rounded-none rounded-e-lg bg-red-50 border text-red-900 placeholder-red-400 focus:outline-none text-base  block flex-1 min-w-0 w-full border-red-300 p-2.5"
+                  placeholder="Hasło"
                 />
               </div>
-              <Button type="submit" className="loginFormBackground bg-[#ff828a] text-[#fafafa] hover:bg-[#ff5a6b] transition-all duration-300 ease-in-out rounded-lg text-lg televisor:text-xl py-3 shadow-md">
-                Zaloguj się
-              </Button>
+              <Button type="submit" className="rounded-none rounded-e-lg bg-red-50 border text-red-900 placeholder-red-400 focus:outline-none hover:bg-red-200 text-base  block flex-1 min-w-0 w-full border-red-300 p-2.5">Zaloguj się</Button>
             </form>
+            <p className="text-gray-100 max-w-sm mx-auto font-semibold text-center text-base">
+            © {nowYear} Teb <span>Technikum</span> i <span>Liceum</span> Wszelkie prawa zastrzeżone.
+            </p>
           </div>
         </div>
-        <p className="text-gray-100 max-w-sm mx-auto font-semibold text-center text-base televisor:text-xl">
-          © {nowYear} Teb <span>Technikum</span> i <span>Liceum</span> Wszelkie prawa zastrzeżone.
-        </p>
-        
       </div>
-      
     </div>
   );
 }
--- src/components/ui/LoginPage/LoginSidePanel.tsx
+++ src/components/ui/LoginPage/LoginSidePanel.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
-import LogoV1 from '../../../../public/teb.avif';
-import LogoV2 from '../../../../public/teb2.avif';
+import LogoV1 from '../../../../public/teb.png';
+import LogoV2 from '../../../../public/teb2.png';
 import { DotPattern } from '@/components/magicui/dot-pattern';
 import { GridPattern } from '@/components/magicui/grid-pattern';
 
@@ -9,7 +9,7 @@ export default function LoginGraphic() {
         <div className="col-span-12 lg:col-span-6 lg:flex relative  h-full z-[-1] lg:h-screen flex-col hidden justify-center items-center bgLogin ">
           
         <DotPattern className="stroke-[#ff828a] fill-[#ff828a] [mask-image:radial-gradient(300px_circle_at_top_right,white,transparent)] xl:[mask-image:radial-gradient(800px_circle_at_top_right,white,transparent)]" />
-        <GridPattern className="stroke-[#ff828a] fill-[#ff828a] [mask-image:radial-gradient(300px_circle_at_bottom_left,white,transparent)] xl:[mask-image:radial-gradient(600px_circle_at_bottom_left,white,transparent)]" />
+        <GridPattern x={12} className="stroke-[#ff828a] fill-[#ff828a] [mask-image:radial-gradient(300px_circle_at_bottom_left,white,transparent)] xl:[mask-image:radial-gradient(600px_circle_at_bottom_left,white,transparent)]" />
         <Image
           src={LogoV2}
           alt="Teb Technikum i Liceum"
@@ -26,8 +26,8 @@ export default function LoginGraphic() {
         />
         <div className="relative justify-center items-center flex w-full py-8">
           <div className="flex flex-col gap-5 justify-center items-center">
-            <h1 className="lg:text-4xl televisor:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-br  from-white to-[#fafafa] bg-opacity-50">
-              Teb Technikum i Liceum
+            <h1 className="lg:text-4xl televisor:text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-br  from-white to-[#fafafa] bg-opacity-50">
+              Teb Technikum i Liceum Łódź
             </h1>
             <div className="flex  relative text-center mx-auto items-start">
               <p className="hidden xl:flex text-[54px] text-[#fafafa] absolute -top-1.5 -left-5">
