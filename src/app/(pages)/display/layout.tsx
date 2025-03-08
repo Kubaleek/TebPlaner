d108b7bdeeb952bf46509105357c774d203148b7
Kuba <kuba.krol.ldz@gmail.com>
2025-03-08 12:30:39 +0100

    poprawki w spacingu

--- src/app/(pages)/display/layout.tsx
+++ src/app/(pages)/display/layout.tsx
@@ -7,7 +7,7 @@ export default function DisplayLayout({ads, advertisements, planner}:
   return (
     <div className="p-4 televisor:p-[40px]  bg-[#FE444C] min-h-screen h-full flex items-center justify-center">
       <div className="grid grid-cols-12 gap-6 lg:gap-[24px]">
-        <div className="col-span-12 lg:col-span-6 flex flex-col gap-8 h-[800px] televisor:h-[1000px]">
+        <div className="col-span-12 lg:col-span-6 gap-6 flex flex-col h-[800px] televisor:h-[1000px]">
           {advertisements}
           {ads}
         </div>
